e5047e8248d792a834c55a2a4d03505eef79fe3a
klimkin-vlad <97385900+klimkin-vlad@users.noreply.github.com>
2025-03-22 12:56:00 +0300

    Update setup.py
    Ссылки/Links

--- setup.py
+++ setup.py
@@ -21,10 +21,10 @@ setup(
         # This is requred, but srmrpy pull it in,
 	    # and there is a pip3 conflict if we have the following
 	    # line.
-        #'gammatone @ git+https://github.com/detly/gammatone',
-        'pypesq @ git+https://github.com/vBaiCai/python-pesq',
-        'srmrpy @ git+https://github.com/jfsantos/SRMRpy',
-        'pesq @ git+https://github.com/ludlows/python-pesq',
+        #'gammatone',
+        'pypesq',
+        'srmrpy',
+        'pesq',
     ],
     include_package_data=True
 )
